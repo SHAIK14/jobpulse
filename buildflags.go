dc056537a1d754c45d5bfc77e2d07c67771d9f66
shaik asif <smd.20sa@gmail.com>
2026-05-10 17:59:05 +0530

    subcommands

--- buildflags.go
+++ buildflags.go
@@ -3,18 +3,35 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 )
 
 func buildflags() {
 
-	wordptr := flag.String("word", "asif", "a name")
-	numptr := flag.Int("age", 25, " a integer of age")
-	adultptr := flag.Bool("adult", true, " above 18")
+	addCmd := flag.NewFlagSet("add", flag.ExitOnError)
+	companyptr := addCmd.String("companyname", "google", "name of the company")
+	joblinkptr := addCmd.String("joblink", "link", "link of the job role")
 
-	flag.Parse()
+	listCmd := flag.NewFlagSet("list", flag.ExitOnError)
 
-	fmt.Println("word:", *wordptr)
-	fmt.Println("age:", *numptr)
-	fmt.Println("adult:", *adultptr)
+	if len(os.Args) < 2 {
+		fmt.Println("expected 'add' or 'list' commands ")
+		os.Exit(1)
+	}
+	switch os.Args[1] {
 
+	case "add":
+		addCmd.Parse(os.Args[2:])
+		fmt.Println("add command")
+		fmt.Println("name:", *companyptr)
+		fmt.Println("joblink:", *joblinkptr)
+	case "list":
+		listCmd.Parse(os.Args[2:])
+		fmt.Println("list command")
+
+	default:
+		fmt.Println("expected add or list comands")
+		os.Exit(1)
+
+	}
 }
